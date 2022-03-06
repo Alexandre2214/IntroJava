3af1dd293170fc6d357d47ae50daca7f25be62cb
Alexandre Cruz <44717399+Alexandre2214@users.noreply.github.com>
2022-03-06 20:05:15 -0300

    Update Programa01.java

--- Programa01.java
+++ Programa01.java
@@ -3,7 +3,14 @@ package Primeiro;
 public class Programa01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	/	int valor;
+		
+		int num1, num2, num3;
+		
+		int num4, num5 = 4, num6 = 7, num7;
+		
+		char caractere;
+		float preco;/ TODO Auto-generated method stub
 
 	}
 
