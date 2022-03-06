3b5f546d50a68ea96639286840a8a172b546bf9d
Alexandre Cruz <44717399+Alexandre2214@users.noreply.github.com>
2022-03-06 20:00:53 -0300

    Conceito básico
    Conceito básico de declaração de variáveis e a utilização de Else If.

---
+++ Programa02.java
@@ -0,0 +1,13 @@
+package Primeiro;
+
+public class Programa02 {
+
+	public static void main(String[] args) {
+		
+		int idade = 38;
+		
+		System.out.println(" Eu tenho " + idade + " anos ");
+
+	}
+
+}
---
+++ Programa04.java
@@ -0,0 +1,19 @@
+package Primeiro;
+
+public class Programa04 {
+
+	public static void main(String[] args) {
+		int numero = 5;
+		
+		if(numero > 5) {
+			
+			System.out.println(" Sim, o numero " + numero + " é maior que 5 ");
+		}else if( numero == 5 ){
+			System.out.println(" O numero " + numero + " é igual a 5 ");
+			}else {
+		System.out.println(" Não, o numero " + numero + " é menor que 5 ");
+		}
+
+	}
+
+}
